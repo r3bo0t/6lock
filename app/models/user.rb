c9712d4d9d2b8c8a6efde4c85cc49ccd9c1a508f
neil <neil@neil.(none)>
2012-08-15 02:10:12 +0200

    Added and began to configure :lockable and :timeoutable devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # and :omniauthable
+  devise :database_authenticatable, :registerable, :lockable,
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
@@ -36,9 +36,9 @@ class User
   # field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
   ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
+  field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
+  field :locked_at,       :type => Time
 
   ## Token authenticatable
   # field :authentication_token, :type => String
