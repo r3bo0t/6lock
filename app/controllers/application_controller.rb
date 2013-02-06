2a92aa3724b4062234ba5e19ea3c1f49e618cd33
Neil <neil.rosenstech@gmail.com>
2013-02-06 17:58:12 -0500

    Sign up page design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def layout_by_resource
-      if devise_controller? && resource_name == :user && action_name == 'new'
+      if devise_controller? && resource_name == :user && %w(new create).include?(action_name)
         "unlocking"
       else
         "application"
