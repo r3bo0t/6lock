86bbc6f8d02209fb3c8e15b3213cccd512e25624
Neil <neil.rosenstech@gmail.com>
2013-02-21 20:12:09 -0500

    Had to do a little cleanup and reformat things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def set_head(title, description = "")
     content_for :head do
-      content_tag(:title, "6LOCK - #{title}") + "<meta name='description' content='#{description}' />".html_safe
+      content_tag(:title, "6LOCK - #{title}") + "<meta name='description' content='#{description}'>".html_safe
     end
   end
 
