9fea5892b9a75d19e70b8bb8a5094f830972b5f2
Neil <neil.rosenstech@gmail.com>
2013-02-21 19:48:23 -0500

    Added a title and a description on pages through handy helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def set_head(title, description = "")
+    content_for :head do
+      content_tag(:title, "6LOCK - #{title}") + "<meta name='description' content='#{description}' />".html_safe
+    end
+  end
+
 end
